df617b1a6a4e61adce31fb5112b2195fbd6e7494
shivalee12 <shah.14@iitj.ac.in>
2023-09-05 18:32:48 +0530

    hyperparameter tuning corrected

--- exp.py
+++ exp.py
@@ -17,7 +17,13 @@ import matplotlib.pyplot as plt
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
-from utils import preprocess_data, split_data, train_model, predict_and_eval, hyperparameter_tuning
+from utils import (
+    preprocess_data,
+    split_data,
+    train_model,
+    predict_and_eval,
+    hyperparameter_tuning,
+)
 
 
 digits = datasets.load_digits()
@@ -33,34 +39,48 @@ digits = datasets.load_digits()
 
 
 # Split data into train and test subsets
-X_train, X_test, X_dev, y_train, y_test, y_dev = split_data(digits.data, digits.target, 0.3, 0.2)
+X_train, X_test, X_dev, y_train, y_test, y_dev = split_data(
+    digits.data, digits.target, 0.3, 0.2
+)
 X_train = preprocess_data(X_train)
 X_test = preprocess_data(X_test)
 
 # Train the model
-model = train_model(X_train, y_train, {'gamma': 0.001}, model_type="svm")
+model = train_model(X_train, y_train, {"gamma": 0.001}, model_type="svm")
 
 # Call the predict_and_eval function
 predict_and_eval(model, X_test, y_test)
 
 # Show the plots
 plt.show()
-plt.savefig('confusion_matrix.png')
+plt.savefig("confusion_matrix.png")
 
 # hyperparameter tuning
 
 gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
 C_ranges = [0.1, 1, 2, 5, 10]
-test_sizes = [0.1, 0.2, 0.3] 
+test_sizes = [0.1, 0.2, 0.3]
 dev_sizes = [0.1, 0.2, 0.3]
 
 for test_size in test_sizes:
     for dev_size in dev_sizes:
         train_size = 1 - test_size - dev_size
-        X_train, X_test, Y_train, Y_test = train_test_split(digits.data, digits.target, test_size=test_size, random_state=42)
-        X_dev, X_test, Y_dev, Y_test = train_test_split(X_test, Y_test, test_size=dev_size / (dev_size + test_size), random_state=42)
+        X_train, X_test, X_dev, y_train, y_test, y_dev = split_data(
+            digits.data, digits.target, test_size, dev_size, random_state=1
+        )
+        # print("X_train shape:", X_train.shape)
+        # print("y_train shape:", y_train.shape)
+        # print("X_dev shape:", X_dev.shape)
+        # print("y_dev shape:", y_dev.shape)
+        best_model, optimal_gamma, optimal_C = hyperparameter_tuning(
+            X_train, y_train, X_dev, y_dev, gamma_ranges, C_ranges
+        )
 
-        best_hparams, best_model, best_accuracy = hyperparameter_tuning(X_train, y_train, X_dev, y_dev, gamma_ranges, C_ranges)
+        train_acc = predict_and_eval(best_model, X_train, y_train)
+        dev_acc = predict_and_eval(best_model, X_dev, y_dev)
+        test_acc = predict_and_eval(best_model, X_test, y_test)
 
-        print(f"test_size={test_size} dev_size={dev_size} train_size={train_size} train_acc={best_accuracy} dev_acc={best_accuracy} test_acc={best_accuracy}")
-        print(f"Best Hyperparameters: {best_hparams}\n")
+        print(
+            f"test_size={test_size} dev_size={dev_size} train_size={train_size} train_acc={train_acc} dev_acc={dev_acc} test_acc={test_acc}"
+        )
+        # print(f"Best Hyperparameters: {best_hparams}\n")
--- utils.py
+++ utils.py
@@ -11,24 +11,31 @@ def preprocess_data(data):
     data = data.reshape((n_samples, -1))
     return data
 
+
 def split_data(x, y, test_size, dev_size, random_state=1):
     X_train, X_temp, y_train, y_temp = train_test_split(
         x, y, test_size=(test_size + dev_size), shuffle=False, random_state=random_state
     )
     test_size_adjusted = test_size / (test_size + dev_size)
     X_test, X_dev, y_test, y_dev = train_test_split(
-        X_temp, y_temp, test_size=test_size_adjusted, shuffle=False, random_state=random_state
+        X_temp,
+        y_temp,
+        test_size=test_size_adjusted,
+        shuffle=False,
+        random_state=random_state,
     )
     return X_train, X_test, X_dev, y_train, y_test, y_dev
 
-def train_model(x,y,model_params,model_type="svm"):
+
+def train_model(x, y, model_params, model_type="svm"):
     if model_type == "svm":
         clf = svm.SVC
-    
+
     model = clf(**model_params)
     # pdb.set_trace()
     model.fit(x, y)
     return model
+
 
 def predict_and_eval(model, X_test, y_test):
     """
@@ -45,48 +52,53 @@ def predict_and_eval(model, X_test, y_test):
     # Predict the value of the digit on the test subset
     predicted = model.predict(X_test)
 
-    # Visualize the first 4 test samples and show their predicted digit value
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(8, 8)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title(f"Prediction: {prediction}")
-        
-    plt.savefig("predicted_digits.png")
-    # Print the classification report
-    print(
-        f"Classification report for classifier {model}:\n"
-        f"{metrics.classification_report(y_test, predicted)}\n"
-    )
+    # # Visualize the first 4 test samples and show their predicted digit value
+    # _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    # for ax, image, prediction in zip(axes, X_test, predicted):
+    #     ax.set_axis_off()
+    #     image = image.reshape(8, 8)
+    #     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    #     ax.set_title(f"Prediction: {prediction}")
 
-    # Plot the confusion matrix
-    disp = metrics.ConfusionMatrixDisplay.from_estimator(model, X_test, y_test)
-    disp.figure_.suptitle("Confusion Matrix")
-    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+    # plt.savefig("predicted_digits.png")
+    # # Print the classification report
+    # print(
+    #     f"Classification report for classifier {model}:\n"
+    #     f"{metrics.classification_report(y_test, predicted)}\n"
+    # )
+
+    # # Plot the confusion matrix
+    # disp = metrics.ConfusionMatrixDisplay.from_estimator(model, X_test, y_test)
+    # disp.figure_.suptitle("Confusion Matrix")
+    # print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
     # If needed, rebuild classification report from confusion matrix
     # The ground truth and predicted lists
-    y_true = []
-    y_pred = []
-    cm = disp.confusion_matrix
+    # y_true = []
+    # y_pred = []
+    # cm = disp.confusion_matrix
 
-    # For each cell in the confusion matrix, add the corresponding ground truths
-    # and predictions to the lists
-    for gt in range(len(cm)):
-        for pred in range(len(cm)):
-            y_true += [gt] * cm[gt][pred]
-            y_pred += [pred] * cm[gt][pred]
+    # # For each cell in the confusion matrix, add the corresponding ground truths
+    # # and predictions to the lists
+    # for gt in range(len(cm)):
+    #     for pred in range(len(cm)):
+    #         y_true += [gt] * cm[gt][pred]
+    #         y_pred += [pred] * cm[gt][pred]
 
-    print(
-        "Classification report rebuilt from confusion matrix:\n"
-        f"{metrics.classification_report(y_true, y_pred)}\n"
-    )
+    # print(
+    #     "Classification report rebuilt from confusion matrix:\n"
+    #     f"{metrics.classification_report(y_true, y_pred)}\n"
+    # )
+    return metrics.accuracy_score(y_test, predicted)
 
 
 def hyperparameter_tuning(X_train, y_train, X_dev, y_dev, gamma_ranges, C_ranges):
     # Define a list of hyperparameter combinations
-    param_combinations = [{'gamma': cur_gamma, 'C': cur_C} for cur_gamma in gamma_ranges for cur_C in C_ranges]
+    param_combinations = [
+        {"gamma": cur_gamma, "C": cur_C}
+        for cur_gamma in gamma_ranges
+        for cur_C in C_ranges
+    ]
 
     best_acc_so_far = -1
     best_model = None
@@ -96,18 +108,18 @@ def hyperparameter_tuning(X_train, y_train, X_dev, y_dev, gamma_ranges, C_ranges
     for params in param_combinations:
         # Train model with the current hyperparameters
         cur_model = train_model(X_train, y_train, params)
-        
+
         # Get accuracy on the dev set
         cur_accuracy = predict_and_eval(cur_model, X_dev, y_dev)
-        
+
         # Check if the current model performs better than the previous best
         if cur_accuracy > best_acc_so_far:
-            print("New best accuracy:", cur_accuracy)
+            # print("New best accuracy:", cur_accuracy)
             best_acc_so_far = cur_accuracy
-            optimal_gamma = params['gamma']
-            optimal_C = params['C']
+            optimal_gamma = params["gamma"]
+            optimal_C = params["C"]
             best_model = cur_model
 
     print("Optimal parameters gamma:", optimal_gamma, "C:", optimal_C)
-    
+
     return best_model, optimal_gamma, optimal_C
